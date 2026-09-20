2d02624b6e15b4a93f3eb8a3e76fd8a62a186e8b
foxsi4696-cmd <example@gmail.com>
2026-09-21 01:26:38 +0500

    Debug Vercel API request

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,6 @@
 const handler = require('../server.js');
 
-module.exports = (req, res) => handler(req, res);
+module.exports = (req, res) => {
+  console.log('VERCEL REQUEST:', req.method, req.url);
+  return handler(req, res);
+};
